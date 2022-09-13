c92d7d6cfc90b8b69b3b4b29686d92ba9bebd070
Megha Bodke <megha.n.bodke@gmail.com>
2022-09-13 15:20:40 +0530

    Add header

---
+++ src/display.js
@@ -0,0 +1,8 @@
+const display = async () => {
+  try {
+    const response = await fetch(
+      www.themealdb.com / api / json / v1 / 1 / categories.php
+    );
+    const { result } = await response.json();
+  } catch (error) {}
+};
