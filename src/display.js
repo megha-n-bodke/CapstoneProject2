39cbd31ccba342101985a89470868aaad2036a50
sonickmumba <kingellie.mumba@gmail.com>
2022-09-13 13:39:58 +0200

    Fix linter errors

--- src/display.js
+++ src/display.js
@@ -1,7 +1,7 @@
 const display = async () => {
   try {
     const response = await fetch(
-      www.themealdb.com / api / json / v1 / 1 / categories.php
+      www.themealdb.com / api / json / v1 / 1 / categories.php,
     );
     const { result } = await response.json();
   } catch (error) {}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-import "./index.css";
-console.log("webpack js");
+import './index.css';
+
+console.log('webpack js');
