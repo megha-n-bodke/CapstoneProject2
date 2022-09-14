6d3eeb947838e72f0ef8b426b0a9e45998195e2d
Megha Bodke <megha.n.bodke@gmail.com>
2022-09-14 15:16:48 +0530

    Style card

--- src/modules/display.js
+++ src/modules/display.js
@@ -20,32 +20,58 @@ export const display = async () => {
 
       const card = document.createElement("div");
       card.className = "card";
+      card.className = "p-3";
 
       const img = document.createElement("img");
       img.className = "card-img-top";
       img.src = strMealThumb;
 
+      //card body
       const cardBodyDiv = document.createElement("div");
       cardBodyDiv.className = "card-body";
 
-      const cardNameLikesDiv = document.createElement("div");
-
       const dishNamePara = document.createElement("p");
       dishNamePara.className = "card-text";
-
-      const cardLikesDiv = document.createElement("div");
-      const likesPara = document.createElement("p");
-
-      const buttonDiv = document.createElement("div");
-      const commentBtn = document.createElement("button");
       dishNamePara.innerText = strMeal;
 
-      singlecard.appendChild(img);
-      singlecard.appendChild(dishNamePara);
+      const cardLikesDiv = document.createElement("div");
+      cardLikesDiv.className = "d-flex";
+      //cardLikesDiv.className = "justify-content-between";
 
-      /* cardContent += `dishNamePara`;
-      singlecard.innerHTML = cardContent;
-       */ //return card;
+      //div for likes
+      const cardNameLikesDiv = document.createElement("div");
+      const likesPara = document.createElement("p");
+      likesPara.innerText = "Likes 0";
+
+      const heart = document.createElement("i");
+      heart.className = "fa fa-heart";
+
+      //button div
+      const buttonDiv = document.createElement("div");
+      const commentBtn = document.createElement("button");
+      buttonDiv.className = "card-buttons";
+      commentBtn.className = "btn";
+      commentBtn.className = "btn-secondary";
+
+      commentBtn.className = "w-25";
+      commentBtn.innerText = "Comments";
+
+      dishNamePara.innerText = strMeal;
+
+      singlecard.appendChild(columnsDiv);
+      columnsDiv.appendChild(card);
+      card.appendChild(img);
+      card.appendChild(cardLikesDiv);
+      cardLikesDiv.appendChild(dishNamePara);
+
+      cardLikesDiv.appendChild(cardNameLikesDiv);
+      cardNameLikesDiv.appendChild(likesPara);
+      cardNameLikesDiv.appendChild(heart);
+
+      card.appendChild(buttonDiv);
+      buttonDiv.appendChild(commentBtn);
+
+      //cardBodyDiv.appendChild();
     });
   } catch (error) {
     console.error(error.message);
