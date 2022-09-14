5acb5c77b1c81e011525b6639ad0f0b596e839f4
sonickmumba <kingellie.mumba@gmail.com>
2022-09-14 21:48:10 +0200

    Add css styles to the comment popup window

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './index.css';
 import display from './modules/display.js';
 
-console.log('webpack js');
+// console.log('webpack js');
 
 document.addEventListener('DOMContentLoaded', () => {
   display();
