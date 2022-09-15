08642377c95cdb11b4496d92e03686d602293024
Megha Bodke <megha.n.bodke@gmail.com>
2022-09-15 18:08:46 +0530

    Fixed linter error

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,5 +1,5 @@
-import { baseUrl, singlecard } from './getElements.js';
-import availableDishCount from './dishCount.js';
+import { baseUrl, singlecard } from "./getElements.js";
+import availableDishCount from "./dishCount.js";
 
 const display = async () => {
   try {
@@ -11,42 +11,42 @@ const display = async () => {
       // @sonick include idMeal in line number 12 with strMeal i have removed due to linter error.
       const { strMeal, strMealThumb } = dish;
 
-      const columnsDiv = document.createElement('div');
-      columnsDiv.className = 'col-md-4';
+      const columnsDiv = document.createElement("div");
+      columnsDiv.className = "col-md-4";
 
-      const card = document.createElement('div');
-      card.className = 'card p-2 my-3 ';
+      const card = document.createElement("div");
+      card.className = "card p-2 my-3 ";
 
-      const img = document.createElement('img');
-      img.className = 'card-img-top';
+      const img = document.createElement("img");
+      img.className = "card-img-top";
       img.src = strMealThumb;
 
       // card body
-      const cardBodyDiv = document.createElement('div');
-      cardBodyDiv.className = 'card-body';
+      const cardBodyDiv = document.createElement("div");
+      cardBodyDiv.className = "card-body";
 
-      const dishNamePara = document.createElement('p');
-      dishNamePara.className = 'card-text';
+      const dishNamePara = document.createElement("p");
+      dishNamePara.className = "card-text";
       dishNamePara.innerText = strMeal;
 
-      const cardLikesDiv = document.createElement('div');
-      cardLikesDiv.className = 'd-flex justify-content-between';
+      const cardLikesDiv = document.createElement("div");
+      cardLikesDiv.className = "d-flex justify-content-between";
 
       // div for likes
-      const cardNameLikesDiv = document.createElement('div');
-      const likesPara = document.createElement('p');
-      likesPara.innerText = 'Likes 0';
+      const cardNameLikesDiv = document.createElement("div");
+      const likesPara = document.createElement("p");
+      likesPara.innerText = "";
 
-      const heart = document.createElement('i');
-      heart.className = 'fa fa-heart';
+      const heart = document.createElement("i");
+      heart.className = "fa fa-heart";
 
       // button div
-      const buttonDiv = document.createElement('div');
-      const commentBtn = document.createElement('button');
-      buttonDiv.className = 'card-actions';
-      commentBtn.className = 'btn btn-secondary';
+      const buttonDiv = document.createElement("div");
+      const commentBtn = document.createElement("button");
+      buttonDiv.className = "card-actions";
+      commentBtn.className = "btn btn-secondary";
 
-      commentBtn.innerText = 'Comments';
+      commentBtn.innerText = "Comments";
 
       dishNamePara.innerText = strMeal;
 
