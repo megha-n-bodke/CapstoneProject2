d614bdc18e76f2ffd79646553074f1dba0f8b123
Megha Bodke <megha.n.bodke@gmail.com>
2022-09-15 19:39:00 +0530

    Add display likes

--- src/modules/postLikes.js
+++ src/modules/postLikes.js
@@ -1,18 +1,27 @@
-import { involvementUrlLikes } from './getElements.js';
+import { involvementUrlLikes } from "./getElements.js";
 
 const postLikes = async (id) => {
   try {
     await fetch(involvementUrlLikes, {
-      method: 'POST',
+      method: "POST",
 
       headers: {
-        'Content-type': 'application/json',
+        "Content-type": "application/json",
       },
 
       body: JSON.stringify({
         item_id: id,
       }),
     });
+    const likeCount = await fetch(involvementUrlLikes);
+    const likes = await likeCount.json();
+    const findId = likes.find((like) => like.item_id === id);
+    const likesPara = document.createElement("p");
+    if (findId === undefined) {
+      likesPara.innerText = "Likes 0";
+    } else {
+      likesPara.innerText = `Likes ${findId.likes}`;
+    }
   } catch (error) {
     console.error(error.message);
   }
