57f02d720a46ebef090c881e740c714297abf7f8
Megha Bodke <megha.n.bodke@gmail.com>
2022-09-16 14:41:35 +0530

    Remove unwanted comment

--- src/modules/display.js
+++ src/modules/display.js
@@ -1,6 +1,6 @@
-import { baseUrl, singlecard, involvementUrlLikes } from './getElements.js';
-import availableDishCount from './dishCount.js';
-import postLikes from './postLikes.js';
+import { baseUrl, singlecard, involvementUrlLikes } from "./getElements.js";
+import availableDishCount from "./dishCount.js";
+import postLikes from "./postLikes.js";
 // import { baseUrl, singlecard } from "./getElements.js";
 // import availableDishCount from "./dishCount.js";
 
@@ -15,44 +15,43 @@ const display = async () => {
     const likes = await likeCount.json();
 
     meals.map((dish) => {
-      // @sonick include idMeal in line number 12 with strMeal i have removed due to linter error.
       const { idMeal, strMeal, strMealThumb } = dish;
 
-      const columnsDiv = document.createElement('div');
-      columnsDiv.className = 'col-md-4';
+      const columnsDiv = document.createElement("div");
+      columnsDiv.className = "col-md-4";
 
-      const card = document.createElement('div');
-      card.className = 'card p-2 my-3 ';
+      const card = document.createElement("div");
+      card.className = "card p-2 my-3 ";
 
-      const img = document.createElement('img');
-      img.className = 'card-img-top';
+      const img = document.createElement("img");
+      img.className = "card-img-top";
       img.src = strMealThumb;
 
       // card body
-      const cardBodyDiv = document.createElement('div');
-      cardBodyDiv.className = 'card-body';
+      const cardBodyDiv = document.createElement("div");
+      cardBodyDiv.className = "card-body";
 
-      const dishNamePara = document.createElement('p');
-      dishNamePara.className = 'card-text';
+      const dishNamePara = document.createElement("p");
+      dishNamePara.className = "card-text";
       dishNamePara.innerText = strMeal;
 
-      const cardLikesDiv = document.createElement('div');
-      cardLikesDiv.className = 'd-flex justify-content-between';
+      const cardLikesDiv = document.createElement("div");
+      cardLikesDiv.className = "d-flex justify-content-between";
 
       // div for likes
       const findId = likes.find((like) => like.item_id === idMeal);
 
-      const cardNameLikesDiv = document.createElement('div');
-      const likesPara = document.createElement('p');
+      const cardNameLikesDiv = document.createElement("div");
+      const likesPara = document.createElement("p");
       if (findId === undefined) {
-        likesPara.innerText = 'Likes 0';
+        likesPara.innerText = "Likes 0";
       } else {
         likesPara.innerText = `Likes ${findId.likes}`;
       }
 
-      const heart = document.createElement('i');
-      heart.className = 'fa fa-heart';
-      heart.addEventListener('click', () => {
+      const heart = document.createElement("i");
+      heart.className = "fa fa-heart";
+      heart.addEventListener("click", () => {
         postLikes(idMeal, likesPara);
       });
 
@@ -64,12 +63,12 @@ const display = async () => {
       // heart.className = "fa fa-heart";
 
       // button div
-      const buttonDiv = document.createElement('div');
-      const commentBtn = document.createElement('button');
-      buttonDiv.className = 'card-actions';
-      commentBtn.className = 'btn btn-secondary';
+      const buttonDiv = document.createElement("div");
+      const commentBtn = document.createElement("button");
+      buttonDiv.className = "card-actions";
+      commentBtn.className = "btn btn-secondary";
 
-      commentBtn.innerText = 'Comments';
+      commentBtn.innerText = "Comments";
 
       dishNamePara.innerText = strMeal;
 
