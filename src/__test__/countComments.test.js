e531d409d193b110b16c99f373bc524cad000f8b
sonickmumba <kingellie.mumba@gmail.com>
2022-09-16 21:19:46 +0200

    Add test for comment count function

---
+++ src/__test__/countComments.test.js
@@ -0,0 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
+
+global.fetch = () => Promise.resolve({
+  json: () => Promise.resolve([
+    {
+      item_id: 52977,
+    },
+  ]),
+});
+test('Check comment counts for meal', async () => {
+  const data = await fetch('https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/iIM70JGWh9jMYmdexC2s/comments?item_id=52977');
+  const comments = await data.json();
+  const numberOfComments = comments.length;
+  expect(comments).toHaveLength(numberOfComments);
+});
