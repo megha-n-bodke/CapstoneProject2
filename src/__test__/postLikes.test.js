c9d1d6c597db134b11479868ee54e30e7dcedff2
Megha Bodke <megha.n.bodke@gmail.com>
2022-09-16 19:23:54 +0530

    Fix linter error

--- src/__test__/postLikes.test.js
+++ src/__test__/postLikes.test.js
@@ -1,17 +1,15 @@
-global.fetch = () =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve([
-        {
-          item_id: 1,
-        },
-      ]),
-  });
-test("Check likes count for dish", async () => {
+global.fetch = () => Promise.resolve({
+  json: () => Promise.resolve([
+    {
+      item_id: 1,
+    },
+  ]),
+});
+test('Check likes count for dish', async () => {
   const data = await fetch(
-    "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/aTMYq3Wixuk5kWkomKWK/likes"
+    'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/aTMYq3Wixuk5kWkomKWK/likes',
   );
   const likes = await data.json();
-  let number = likes.length;
+  const number = likes.length;
   expect(likes).toHaveLength(number);
 });
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,6 +1,6 @@
-import { baseUrl, singlecard, involvementUrlLikes } from "./getElements.js";
-import availableDishCount from "./dishCount.js";
-import postLikes from "./postLikes.js";
+import { baseUrl, singlecard, involvementUrlLikes } from './getElements.js';
+import availableDishCount from './dishCount.js';
+import postLikes from './postLikes.js';
 
 const display = async () => {
   try {
@@ -8,7 +8,7 @@ const display = async () => {
     const { meals } = await response.json();
     let dishCount = 0;
 
-    //likes
+    // likes
     const likeCount = await fetch(involvementUrlLikes);
     const likes = await likeCount.json();
 
@@ -16,50 +16,50 @@ const display = async () => {
       // @sonick include idMeal in line number 12 with strMeal i have removed due to linter error.
       const { idMeal, strMeal, strMealThumb } = dish;
 
-      const columnsDiv = document.createElement("div");
-      columnsDiv.className = "col-md-4";
+      const columnsDiv = document.createElement('div');
+      columnsDiv.className = 'col-md-4';
 
-      const card = document.createElement("div");
-      card.className = "card p-2 my-3 ";
+      const card = document.createElement('div');
+      card.className = 'card p-2 my-3 ';
 
-      const img = document.createElement("img");
-      img.className = "card-img-top";
+      const img = document.createElement('img');
+      img.className = 'card-img-top';
       img.src = strMealThumb;
 
       // card body
-      const cardBodyDiv = document.createElement("div");
-      cardBodyDiv.className = "card-body";
+      const cardBodyDiv = document.createElement('div');
+      cardBodyDiv.className = 'card-body';
 
-      const dishNamePara = document.createElement("p");
-      dishNamePara.className = "card-text";
+      const dishNamePara = document.createElement('p');
+      dishNamePara.className = 'card-text';
       dishNamePara.innerText = strMeal;
 
-      const cardLikesDiv = document.createElement("div");
-      cardLikesDiv.className = "d-flex justify-content-between";
+      const cardLikesDiv = document.createElement('div');
+      cardLikesDiv.className = 'd-flex justify-content-between';
 
       // div for likes
       const findId = likes.find((like) => like.item_id === idMeal);
-      const cardNameLikesDiv = document.createElement("div");
-      const likesPara = document.createElement("p");
+      const cardNameLikesDiv = document.createElement('div');
+      const likesPara = document.createElement('p');
       if (findId === undefined) {
-        likesPara.innerText = "Likes 0";
+        likesPara.innerText = 'Likes 0';
       } else {
         likesPara.innerText = `Likes ${findId.likes}`;
       }
 
-      const heart = document.createElement("i");
-      heart.className = "fa fa-heart";
-      heart.addEventListener("click", () => {
+      const heart = document.createElement('i');
+      heart.className = 'fa fa-heart';
+      heart.addEventListener('click', () => {
         postLikes(idMeal, likesPara);
       });
 
       // button div
-      const buttonDiv = document.createElement("div");
-      const commentBtn = document.createElement("button");
-      buttonDiv.className = "card-actions";
-      commentBtn.className = "btn btn-secondary";
+      const buttonDiv = document.createElement('div');
+      const commentBtn = document.createElement('button');
+      buttonDiv.className = 'card-actions';
+      commentBtn.className = 'btn btn-secondary';
 
-      commentBtn.innerText = "Comments";
+      commentBtn.innerText = 'Comments';
 
       dishNamePara.innerText = strMeal;
 
@@ -80,7 +80,7 @@ const display = async () => {
     });
     availableDishCount(dishCount);
   } catch (error) {
-    console.error(error.message);
+    throw new Error(error.message);
   }
 };
 
