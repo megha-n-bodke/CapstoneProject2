0142ddfabc66eb32ef3491bddbd215a9aa5fc046
Megha Bodke <megha.n.bodke@gmail.com>
2022-09-16 16:56:43 +0530

    Add test For Likes Count

--- src/__test__/postLikes.test.js
+++ src/__test__/postLikes.test.js
@@ -1,7 +1,17 @@
+global.fetch = () =>
+  Promise.resolve({
+    json: () =>
+      Promise.resolve([
+        {
+          item_id: 1,
+        },
+      ]),
+  });
 test("Check likes count for dish", async () => {
   const data = await fetch(
     "https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/aTMYq3Wixuk5kWkomKWK/likes"
   );
   const likes = await data.json();
-  expect(likes).toBe();
+  let number = likes.length;
+  expect(likes).toHaveLength(number);
 });
